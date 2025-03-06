df56c4524b34f7ed94da447f69440ed685860f17
justin-swenson111 <justinswenson111@gmail.com>
2025-03-05 17:18:25 -0700

    base game

---
+++ script.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.src = ''; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
